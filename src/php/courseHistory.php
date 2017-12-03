a8f29a05e3b70610c707115221bbc07bd07880f9
KetkiMahajan <mahajanketki24@gmail.com>
2017-12-03 12:05:44 -0600

    menu chnaged
    ketki8

--- src/php/courseHistory.php
+++ src/php/courseHistory.php
@@ -16,8 +16,7 @@ else{
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
   <?php include 'menu.php'; ?>
